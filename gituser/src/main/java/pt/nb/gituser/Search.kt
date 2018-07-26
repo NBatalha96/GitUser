483ab76aa993e4a4f161f31f179f82363e71dd2d
Nuno Batalha <nuno_batalha_1996@hotmail.com>
2018-07-26 15:15:39 +0100

    Commit committed

---
+++ gituser/src/main/java/pt/nb/gituser/Search.kt
@@ -0,0 +1,12 @@
+package pt.nb.gituser
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Search : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.at_search)
+    }
+}
